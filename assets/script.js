c0cb0ddc9ebabcd433ccd3e727646b5c726258d1
Ilona Tsivunchyk <ilona.tsivunchyk@gmail.com>
2023-11-22 15:30:28 +0100

    definitive

--- assets/script.js
+++ assets/script.js
@@ -21,30 +21,28 @@ const slides = [
 const arrowLeft = document.querySelector('.arrow_left');
 const arrowRight = document.querySelector('.arrow_right');
 const carousel=document.getElementById('banner');
-
-let slide = 0;
-
 const carouselImage = carousel.children.item(0);
 const carouselTag = carousel.children.item(1);
 const dotDiv = document.querySelector('.dots');
 const dots= dotDiv.children;
 
+let slide = 0;
 
-//Points
+
+//Dots
 for (let i = 0; i < slides.length-1; i++) {
 	const dot = document.createElement('div');
 	dot.classList.add('dot');
 	dotDiv.appendChild(dot);
 	dot.addEventListener('click', function() {
-	slide = i;
-	
-	carouselImage.src="./assets/images/slideshow/"+slides[slide].image;
-	  carouselTag.innerHTML = slides[slide].tagLine;
-	  dotSelected();
+		slide = i;
+		carouselImage.src="./assets/images/slideshow/"+slides[slide].image;
+		carouselTag.innerHTML = slides[slide].tagLine;
+		dotSelected();
 	});
   }
   
-  function dotSelected() {
+function dotSelected() {
 	for (let i = 0; i < dots.length; i++) {
 	  if (i === slide) {
 		dots[i].classList.add('dot_selected');
@@ -61,8 +59,8 @@ arrowLeft.addEventListener('click', function(){
 		slide=slides.length-1;
 	}
 	carouselImage.src="./assets/images/slideshow/"+slides[slide].image;
-carouselTag.innerHTML=slides[slide].tagLine;
-dotSelected();
+	carouselTag.innerHTML=slides[slide].tagLine;
+	dotSelected();
 })
 
 // Gestionnaire d'événement pour le clic sur la flèche droite //
